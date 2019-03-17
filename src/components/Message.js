5e398d26eded25d795104e96595836fc7ead6334
Gorfad_Vijay <gorfadvijay1234@gmail.com>
2019-03-18 00:50:55 +0530

    Added Message Components

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-class Message extends React.Component {
-  render() {
-    return <div className="message" />;
-  }
+function Message(props) {
+  return (
+    <div className="message">
+      <div className="message-username"> {props.username} </div>
+      <div className="message-text"> {props.text} </div>
+    </div>
+  );
 }
 
 export default Message;
