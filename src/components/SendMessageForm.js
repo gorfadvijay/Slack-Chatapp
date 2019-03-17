4360d28f13d763ce7d27224c0233339487b1d3da
Gorfad_Vijay <gorfadvijay1234@gmail.com>
2019-03-18 00:51:14 +0530

    Added Sedn Message From

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 class SendMessageForm extends React.Component {
+  state = {
+    message: []
+  };
+  handleChange = e => {
+    this.setState({
+      message: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.message);
+  };
   render() {
     return (
-      <form className="send-message-form">
-        <input placeholder="SendMessageForm" type="text" />
+      <form onSubmit={this.handleSubmit} className="send-message-form">
+        <input
+          onChange={this.handleChange}
+          placeholder="Type Your Message and hit enter"
+          type="text"
+        />
       </form>
     );
   }
