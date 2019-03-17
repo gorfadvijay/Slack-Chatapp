b85aa94b21aa1dd9f15d6a4f3e0ac98ea165b173
Gorfad_Vijay <gorfadvijay1234@gmail.com>
2019-03-17 13:03:37 +0530

    Added Basic CHat Comonents

---
+++ src/components/Message.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Message extends React.Component {
+  render() {
+    return <div className="message" />;
+  }
+}
+
+export default Message;
---
+++ src/components/MessageList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+const DUMMY_DATA = [
+  {
+    senderId: 'Vijay',
+    text: 'hello hiii'
+  },
+  {
+    senderId: 'Jd',
+    text: 'hello Bhai'
+  },
+  {
+    senderId: 'Mama',
+    text: 'Bye'
+  }
+];
+
+class MessageList extends React.Component {
+  render() {
+    return (
+      <div className="message-list">
+        {DUMMY_DATA.map((message, index) => {
+          return (
+            <div className="message">
+              <div className="message-username"> {message.senderId}</div>
+              <div className="message-text"> {message.text}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default MessageList;
