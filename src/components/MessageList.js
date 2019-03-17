6961fb9b083e1910cf3b89d0845d1c0bd4b0df08
Gorfad_Vijay <gorfadvijay1234@gmail.com>
2019-03-18 00:50:34 +0530

    Added Message Prop And State

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,29 +1,17 @@
 import React from 'react';
-const DUMMY_DATA = [
-  {
-    senderId: 'Vijay',
-    text: 'hello hiii'
-  },
-  {
-    senderId: 'Jd',
-    text: 'hello Bhai'
-  },
-  {
-    senderId: 'Mama',
-    text: 'Bye'
-  }
-];
+import Message from './Message';
 
 class MessageList extends React.Component {
   render() {
     return (
       <div className="message-list">
-        {DUMMY_DATA.map((message, index) => {
+        {this.props.messages.map((message, index) => {
           return (
-            <div className="message" key={index}>
-              <div className="message-username"> {message.senderId}</div>
-              <div className="message-text"> {message.text}</div>
-            </div>
+            <Message
+              key={index}
+              username={message.senderId}
+              text={message.text}
+            />
           );
         })}
       </div>
