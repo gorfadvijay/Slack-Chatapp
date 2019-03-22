874bd1521b0c05c1491a9c32e3f19eb8a486ea9c
Gorfad_Vijay <gorfadvijay1234@gmail.com>
2019-03-22 17:01:06 +0530

    NewRoom Adder Component

--- src/components/NewRoomForm.js
+++ src/components/NewRoomForm.js
@@ -1,19 +1,37 @@
-import React from 'react'
+import React from 'react';
 
 class NewRoomForm extends React.Component {
-    render () {
-        return (
-            <div className="new-room-form">
-                <form>
-                    <input
-                        type="text" 
-                        placeholder="NewRoomForm" 
-                        required />
-                    <button id="create-room-btn" type="submit">+</button>
-            </form>
-        </div>
-        )
-    }
+  state = {
+    roomName: []
+  };
+  handleChange = e => {
+    this.setState({
+      roomName: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.createRoom(this.state.roomName);
+    this.setState({ roomName: '' });
+  };
+  render() {
+    return (
+      <div className="new-room-form">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleChange}
+            value={this.state.roomName}
+            type="text"
+            placeholder="Add New Room"
+            required
+          />
+          <button id="create-room-btn" type="submit">
+            +
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default NewRoomForm
+export default NewRoomForm;
